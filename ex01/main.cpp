71a47ca51eb227a7ba8c8bf15ae99627c4770ea9
MiniKlar <omontloris@gmail.com>
2025-12-23 21:47:12 +0100

    - Refactored main function for improved readability and added detailed output for pointer addresses during serialization and deserialization

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,21 +6,30 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 00:40:33 by lomont            #+#    #+#             */
-/*   Updated: 2025/12/23 00:57:31 by lomont           ###   ########.fr       */
+/*   Updated: 2025/12/23 21:46:05 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
 #include "Data.hpp"
 
-int main(void) {
-	Data* feur = new Data;
-	Data* oklm;
-	uintptr_t t;
-	std::cout << feur->getName() << std::endl;
-	t = Serializer::serialize(feur);
-	oklm = Serializer::deserialize(t);
-	std::cout << oklm->getName() << std::endl;
-	delete oklm;
+int main( void ) {
+	Data* before = new Data;
+	Data* after;
+	uintptr_t ptr;
+
+	std::cout << "Voici l'adresse du pointeur before = " << before << std::endl;
+	std::cout << before->getName() << std::endl;
+
+	ptr = Serializer::serialize(before);
+	std::cout << "Voici l'adresse uintptr = " << ptr << std::endl;
+
+	if (before == reinterpret_cast<Data*>(ptr))
+		std::cout << "Address are the same: before address: " << before << " ptr address: " << std::hex << ptr << std::endl;
+
+	after = Serializer::deserialize(ptr);
+	std::cout << after->getName() << std::endl;
+
+	delete after;
 	return (0);
 }
